8d562e2fd8d766bdeade8f9111bda93ec3c2a8b2
Magnus Johannessen <magnuswj@stud.ntnu.no>
2023-11-05 15:46:38 +0100

    fix: locals syntax

--- demo-infra/locals.tf
+++ demo-infra/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+  workspaces_suffix = terraform.workspace == "default" ? "" : terraform.workspace
 #Resource Group
   rg_name = terraform.workspace == "default" ? "${var.base_name}-${var.rg_name}" : "${var.base_name}-${var.rg_name}-${local.workspaces_suffix}"
 #Storage account
@@ -9,9 +9,9 @@ locals {
   kv_name = terraform.workspace == "default" ? "${var.base_name}${var.kv_name}${random_string.random_string.result}" : "${var.base_name}${var.kv_name}${random_string.random_string.result}-${local.workspaces_suffix}"
   sa_accesskey_name = terraform.workspace == "default" ? "${var.sa_accesskey_name}${azurerm_storage_account.sa.name}" : "${var.sa_accesskey_name}${azurerm_storage_account.sa.name}-${local.workspaces_suffix}"
 #Virtual network
-    vnet_name = terraform.workspace == "default" ? "${var.vnet_name}" : "${var.vnet_name}-${local.workspaces_suffix}"
-    nsg_name = terraform.workspace == "default" ? "${var.nsg_name}" : "${var.nsg_name}-${local.workspaces_suffix}"
-    subnet_name = terraform.workspace == "default" ? "${var.subnet_name}" : "${var.subnet_name}-${local.workspaces_suffix}"
+    vnet_name = terraform.workspace == "default" ? var.vnet_name : "${var.vnet_name}-${local.workspaces_suffix}"
+    nsg_name = terraform.workspace == "default" ? var.nsg_name : "${var.nsg_name}-${local.workspaces_suffix}"
+    subnet_name = terraform.workspace == "default" ? var.subnet_name : "${var.subnet_name}-${local.workspaces_suffix}"
 
 #virtualmachine
     vm_nic_name = terraform.workspace == "default" ? "${var.base_name}-${var.vm_nic_name}" : "${var.base_name}-${var.vm_nic_name}-${local.workspaces_suffix}"
